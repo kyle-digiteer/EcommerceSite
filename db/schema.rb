c643916844b9458e11ac30b457bc84a0c14af23a
kyle-digiteer <kyle@digiteer.digital>
2024-09-14 21:42:36 +0800

    modify columns in cart item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_14_035306) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_14_041011) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,7 +31,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_14_035306) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "product_id", null: false
     t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
   end
 
   create_table "carts", force: :cascade do |t|
@@ -73,6 +75,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_14_035306) do
   end
 
   add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
   add_foreign_key "carts", "users"
   add_foreign_key "product_variants", "products"
 end
